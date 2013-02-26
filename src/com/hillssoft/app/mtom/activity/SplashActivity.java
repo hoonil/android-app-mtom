39e2898478c60ed0ffbf1900bc9d996b87da1968
Derek.Kang <derek@kakao.com>
2013-02-26 21:11:06 +0900

    임시 수정

--- src/com/hillssoft/app/mtom/activity/SplashActivity.java
+++ src/com/hillssoft/app/mtom/activity/SplashActivity.java
@@ -1,17 +1,24 @@
 package com.hillssoft.app.mtom.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.hillssoft.app.R;
 import com.hillssoft.framework.manager.BaseActivityManager;
 
 public class SplashActivity extends BaseActivityManager {
 
+	int count = 0;
+	TextView txt1 = null;
+	Button btn1 = null;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	
-	
+		onDoStart();
 	}
 	
 	
@@ -19,6 +26,11 @@ public class SplashActivity extends BaseActivityManager {
 	protected void initializeView() {
 		super.initializeView();
 		setContentView(R.layout.mtom_splash_activity);
+		
+		
+		txt1 = (TextView)findViewById(R.id.txt1);
+		btn1 = (Button)findViewById(R.id.btn1);
+		
 		
 	}
 	
@@ -29,8 +41,26 @@ public class SplashActivity extends BaseActivityManager {
 		
 	protected void onDoStart(){
 		
+		
+		
+		
+		
 	}
 
+	
+	
+	
+	public class Run implements Runnable{
+		@Override
+		public void run() {
+			try{
+				Thread.sleep(1000);
+				count++;
+			}catch(InterruptedException e){
+				
+			}
+		}
+	}
 	
 	
 	
